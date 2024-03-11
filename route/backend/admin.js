8acc76afa9fb88141a8ec225c970c4ef1ef5fbdb
Kartik Bhatnagar <72219613+Kartik989-max@users.noreply.github.com>
2024-03-11 23:09:04 +0530

    Update admin.js

--- route/backend/admin.js
+++ route/backend/admin.js
@@ -1,12 +1,61 @@
+// let express = require("express")
+// let pageModel = require('../../model/pageModel')
+// let router = express()
+
+// router.get("/",(req,res)=>{
+//     pageModel.find({}).count()
+//     .then((x)=>{
+//         res.render("../views/backend/admin-file",{x})
+//         // console.log(x)
+//     })
+// })
+// module.exports = router
+
 let express = require("express")
 let pageModel = require('../../model/pageModel')
+const LoginModel = require("../../model/loginModel");
+const bcrypt = require('bcrypt');
+// let LoginModel = require('../../model/loginModel')
 let router = express()
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/",(req,res)=>{
-    pageModel.find({}).count()
-    .then((x)=>{
-        res.render("../views/backend/admin-file",{x})
-        // console.log(x)
-    })
+        res.render("backend/login")
 })
+
+
+router.post("/", async (req, res) => {
+    try {
+        const check = await LoginModel.findOne({ name: req.body.username });
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if (!check && !isPasswordMatch) {
+            res.status(200).send("wrong credentials");
+        }
+        else {
+            console.log('conn');
+            // res.status(200).render("backend/admin-file");
+            pageModel.find({}).count()
+            .then((x)=>{
+                res.render("../views/backend/admin-file",{x})
+                // console.log(x)
+            })
+        }
+        ;
+    }
+    catch {
+        res.status(405).send("wrong Details");
+    }
+});
+
+// router.get("/",(req,res)=>{
+//     pageModel.find({}).count()
+//     .then((x)=>{
+//         res.render("../views/backend/admin-file",{x})
+//         // console.log(x)
+//     })
+// })
+
+
 module.exports = router
