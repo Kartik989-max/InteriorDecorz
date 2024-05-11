877b7aabc8703a2008ea1a43e020de02a8ee3439
Kartik Bhatnagar <72219613+Kartik989-max@users.noreply.github.com>
2024-05-12 02:53:17 +0530

    Create residential.js

---
+++ views/frontent/residential.js
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  
+    <%-include('common/head.ejs')-%>
+  <title>Residential</title>
+</head>
+
+<body style="overflow-x: hidden;">
+
+    <%-include('common/header.ejs')-%>
+
+
+    
+    <div class="img_container top_img">
+      <img
+        src="frontent/images/interior_main2.jpg"
+        alt="" style="filter: brightness(50%);"
+      />
+      <!-- <div class="main-section" style="padding: 0">
+        <h1 style="font-size: 80px">Hello This is Demo</h1>
+      </div> -->
+    </div>
+    <section class="main-section">
+      <div class="content_wrapper">
+        
+        <h1 class="cinzelMain" style="color: rgba(245, 222, 179, 0.716); font-size: 2rem;">CUSTOMIZED AND CHIC
+          ARCHITECTURE</h1>
+        
+        <p class="faunaTextP">
+          Aesthetics is entirely dependent on the architecture at work and no one but the best architect can bring your visions to life. Alive Studio is the brand for the job, as it has been for so many years, where our creative experts resonate with your ideas and paint a picturesque architecture in sync with modern trends. We customize as per your needs. We customize as per your space. Alive studio infuses creativity to every tissue of your intended architecture. <br>
+
+The process we follow to build your architecture is intricate. We first understand your vision. We then draw out a blueprint. Our team constantly communicates with you to perfect the design and exactly copies the plan into the real space. Throughout, Alive Studio prioritizes customer satisfaction and on-time completion. We respect the art in architecture and deliver what we promise. For the best customized and chic architecture, get in touch with us. Quality and beauty await you.
+        </p>
+        <!-- <div class="img_container anime_img" style="margin: 25px 0">
+          <img
+            src="https://images.unsplash.com/photo-1682687982360-3fbab65f9d50?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div> -->
+      
+        
+        </div>
+      </div>
+    </section>
+
+
+
+
+    <div>
+    <div class="main3_text" style="position:absolute !important;
+    font-optical-sizing: auto;
+    top: 50%;
+    left: 20%;
+    color: white;">
+
+<h1 class="text-center">Residential</h1>
+    <p class="text-center faunaTextP" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam ab mollitia dolores hic ipsam. Eveniet nostrum illum in aliquid accusamus harum placeat dolorum eius corrupti.</p>
+    </div>
+   
+   
+  <!-- <img class="img-fluid position-sticky" src="frontent/images/interior_main.jpg" alt=""> -->
+
+  <div class="main4  justify-content-center d-flex">
+    <div class="img">
+      <img src="frontent/images/main04.jpg" style="width:35rem;height: 30rem;" alt="">
+    </div>
+    <div  class="main4_text" data-aos="fade-right">
+      <h1 class="cinzelMain">Concept & Details .</h1>
+      <span style="display: block; height: 1px; width: 100%; position: relative; border: 1px solid rgba(255, 255, 255, 0.527);"></span>
+      <p class="faunaTextP">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis quod ipsum? Sit consequuntur ipsa quia fuga corporis vitae eligendi cum fugiat! Facere deleniti id nemo odit, commodi architecto pariatur perferendis ullam totam reiciendis molestiae necessitatibus voluptatem temporibus nostrum numquam laboriosam. Ad ipsa rem debitis atque quaerat est voluptatem, iste nisi deleniti suscipit autem laborum.
+      </p>
+    </div>
+  </div>
+  
+
+
+
+</body>
+
+<%-include('common/footer.ejs')-%>
+</html>
